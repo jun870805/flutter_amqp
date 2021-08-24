b5005c462bba8e1c6734e48f550d0e83e3030133
jun870805 <jun870805@gmail.com>
2021-08-24 14:11:50 +0800

    v1.0.6

--- lib/provider.dart
+++ lib/provider.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
 
-import 'amqp.dart';
-
 class AMQPMessage with ChangeNotifier {
   String _receiveMessage = "";
 
